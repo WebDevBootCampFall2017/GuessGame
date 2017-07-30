64f0ccd71ca9eee81d7a2b2153a2f133969ba32b
Jeremy <jeremy.freeman@raviolilabs.com>
2017-07-30 14:52:13 -0400

    Initial check-in of module NumberGuessGame

---
+++ NumberGuessGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberGuessGame
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
