5c397c076610b5cf11e9d99bb9ffa5e433da1fd5
LabUser <LabUser@waketech.edu>
2017-08-03 19:38:44 -0400

    Made world famous guessing game all caps

--- NumberGuessGame/Program.cs
+++ NumberGuessGame/Program.cs
@@ -8,7 +8,7 @@ namespace NumberGuessGame
         {
             //I want to remember the username, so we create a var to hold it
             String player_name;
-            Console.WriteLine("Welcome to the World Famous Guessing game!");
+            Console.WriteLine("Welcome to the WORLD FAMOUS GUESSING GAME!");
             Console.WriteLine("What is your name contestent?");
             player_name = Console.ReadLine();
             Console.WriteLine("Lets roll the dice behind the screen, and see what number we get!");
